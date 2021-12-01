366c33685ee1477f8347b5459f16b94b72e37479
Bharat Patodi <bharatpatodi@gmail.com>
2021-12-01 16:52:21 +0530

    Add light/dark theme

--- src/index.js
+++ src/index.js
@@ -1,23 +1,38 @@
 const feedback = document.getElementById('feedback');
 const feedbackSubmit = document.getElementById('feedback-submit');
 const feedbackDisplay = document.getElementById('feedback-display');
+const themeSwitcher = document.getElementById('theme-switcher');
 
 feedbackSubmit.addEventListener('click', (e) => {
     e.preventDefault();
     const feedbackText = feedback.value;
     if (feedbackText.length > 0) {
         feedback.value = '';
-        feedback.classList.remove('error');
-        feedback.classList.add('success');
         feedback.placeholder = 'Thanks for your feedback!';
-        feedbackDisplay.innerHTML = feedbackText;
+        feedbackDisplay.innerText = '->  ' + feedbackText;
         const feedbackColour = document.querySelector(
           'input[name="colour-blocks"]:checked'
         ).value;
         feedbackDisplay.style.color = feedbackColour;
     } else {
-        feedback.classList.remove('success');
-        feedback.classList.add('error');
         feedback.placeholder = 'Please enter your feedback!';
+    }
+});
+
+themeSwitcher.addEventListener('click', (e) => {
+    let headerNavLinks = document.querySelectorAll('.header-nav-link');
+    const theme = document.body.classList;
+    if (theme.contains('dark-theme')) {
+        theme.remove('dark-theme');
+        theme.add('light-theme');
+        headerNavLinks.forEach(link => {
+            link.style.color = 'black';
+        })
+    } else {
+        theme.remove('light-theme');
+        theme.add('dark-theme');
+        headerNavLinks.forEach(link => {
+            link.style.color = '#fff';
+        })
     }
 });
